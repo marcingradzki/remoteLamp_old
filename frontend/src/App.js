50ead326fa0d9ad607aefd62475466b08f73c9e9
Marcin Grądzki <marcin.gradzki@billennium.com>
2019-03-29 11:15:07 +0100

    Basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLightbulb, faClock, faInfoCircle, faCloudMoon } from '@fortawesome/free-solid-svg-icons'
+import Icon from './icon';
+
+library.add(faLightbulb, faClock, faInfoCircle, faCloudMoon);
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 5fr 1fr;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Switch = styled.div`
+  padding-top: 50px;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 class App extends Component {
+
+  state = {
+    icons: {
+      'bulb': {
+        icon: 'lightbulb',
+        width: '40vh',
+        height: '40vh',
+      },
+      'time': {
+        icon: 'clock',
+        width: '10vh',
+        height: '10vh',
+      },
+      'details': {
+        icon: 'info-circle',
+        width: '10vh',
+        height: '10vh',
+      },
+      'sunset': {
+        icon: 'cloud-moon',
+        width: '10vh',
+        height: '10vh',
+      },
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-      </div>
+      <Container>
+        <Switch>
+          <Icon {...this.state.icons.bulb} />
+        </Switch>
+        <Buttons>
+          <Icon {...this.state.icons.details} />
+          <Icon {...this.state.icons.time} />
+          <Icon {...this.state.icons.sunset} />
+        </Buttons>
+      </Container>
     );
   }
 }
---
+++ frontend/src/icon.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const setShadows = glow => {
+    let y1 = glow ? '3' : '10',
+        y2 = glow ? '1' : '6',
+        blur1 = glow ? '7' : '20',
+        blur2 = glow ? '1' : '6';
+    return `0 ${y1}px ${blur1}px rgba(0,0,0,0.19), 0 ${y2}px ${blur2}px rgba(0,0,0,0.23);`
+};
+
+const Icon = styled.svg`
+    width: ${props => props.width};
+    height: ${props => props.height};
+    color: ${props => props.glow ? '#F1D302' : '#CADADF'};
+    border-radius: 15px;
+    box-shadow: ${props => setShadows(props.glow)};
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+    padding: 10px;
+`;
+
+export default ({icon, width, height}) => {
+    const [clicked, setClicked] = React.useState(false);
+    return (
+        <Icon
+            width={width}
+            height={height}
+            glow={clicked}
+            onClick={() => setClicked(!clicked)}
+        >
+            <FontAwesomeIcon icon={icon} />
+        </Icon>
+    );
+};
